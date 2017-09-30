b75521ec6215a8038729b667b00de766bae0fa4e
Eduard Gilmutdinov <edgilmutdinov@gmail.com>
2017-09-30 14:27:39 +0500

    Specify the title and description on About page

--- client/src/About.js
+++ client/src/About.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 
-const About = () => <div>About Page</div>;
+const About = () => (
+  <div>
+    <Helmet>
+      <title>About</title>
+      <meta name="description" content="The page about ..." />
+    </Helmet>
+    About Page
+  </div>
+);
 
 export default About;
